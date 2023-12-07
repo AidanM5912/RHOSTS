45f88a24ecdaaa89f418aaa758689fa7b0eb3eaf
Aidan Morson <85406066+AidanM5912@users.noreply.github.com>
2023-12-06 19:34:58 -0800

    updated for second run going to rerun both of these and see if i can track the output files

--- Example/test_launch_high_order.sh
+++ Example/test_launch_high_order.sh
@@ -2,4 +2,4 @@
 ### Launch the code for the times 0-5 and using 5 cores
 codepath="/Users/aidanmorson/Documents/GitHub/RHOSTS/High_order_TS/test.py"
 filename="/Users/aidanmorson/Desktop/neuro/data/hw-sample-data/Spike_Raster_Control.mat"
-python ${codepath} ${filename} -t 0 5 -p 5
+python ${codepath} ${filename} -t 0 5 -p 5 -s run1
